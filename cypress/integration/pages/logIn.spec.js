c62006830b917a4365f53bba470d9513acb9eac8
Bogdan <bogdan@madetech.com>
2020-11-25 18:34:55 +0200

    Add cypress

---
+++ cypress/integration/pages/logIn.spec.js
@@ -0,0 +1,6 @@
+describe('Loads page', () => {
+    it("has 'Please log in' heading", () => {
+      cy.visit(`http://localhost:3000`);
+      cy.get('h1').should('have.text', "Please log in");
+    });
+  });
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
+import { Container, Header, Heading, HeadingLevels, Link, Main, PhaseBanner, Tile } from 'lbh-frontend-react'
 import Head from 'next/head'
+//import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
+    <div>
+      <Header serviceName="Document Evidence Service">
+        <title>Document Evidence Service</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Header>
+      <Container>
+        <PhaseBanner phase="ALPHA" url="form-url"/>
+      </Container>
+      <Main>
+        <div className="lbh-container">
+          <Heading level={HeadingLevels.H1}>Please log in</Heading>
+          <Tile link="https://auth.hackney.gov.uk/auth?redirect_uri=http://localhost:3000" title="Log in with Google" />
+          {/* <Link href={`https://auth.hackney.gov.uk/auth?redirect_uri=${redirect_uri}`} Login with google></Link> */}
+        </div>
+      </Main>
     </div>
   )
 }
