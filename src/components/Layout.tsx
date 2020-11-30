c7f9f6cc5e863326568af62fbed94fc65daec527
Jaye <joshua.hackett93@gmail.com>
2020-11-30 10:54:23 +0000

    refactor layout into seperate component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Header, Main, PhaseBanner } from 'lbh-frontend-react';
+
+const Layout = (props: Props): JSX.Element => (
+  <>
+    <Header serviceName="Document Evidence Service" />
+    <Container>
+      <PhaseBanner phase="ALPHA" url="form-url" />
+    </Container>
+    <Main>
+      <div className="lbh-container">{props.children}</div>
+    </Main>
+  </>
+);
+
+export interface Props {
+  children: React.ReactNode;
+}
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,16 @@
-import {
-  Container,
-  Header,
-  Heading,
-  HeadingLevels,
-  Main,
-  PhaseBanner,
-  Tile,
-} from 'lbh-frontend-react';
+import { Heading, HeadingLevels, Tile } from 'lbh-frontend-react';
+import Layout from '../Components/Layout';
 import { ReactNode } from 'react';
 
-const Home = (): ReactNode => {
-  return (
-    <div>
-      <Header serviceName="Document Evidence Service">
-        <title>Document Evidence Service</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Header>
-      <Container>
-        <PhaseBanner phase="ALPHA" url="form-url" />
-      </Container>
-      <Main>
-        <div className="lbh-container">
-          <Heading level={HeadingLevels.H1}>Please log in</Heading>
-          <Tile
-            link="https://auth.hackney.gov.uk/auth?redirect_uri=http://localdev.hackney.gov.uk:3000/temp"
-            title="Log in with Google"
-          />
-        </div>
-      </Main>
-    </div>
-  );
-};
+const Home = (): ReactNode => (
+  <Layout>
+    <Heading level={HeadingLevels.H1}>Please log in</Heading>
+    <Tile
+      link="https://auth.hackney.gov.uk/auth?redirect_uri=http://localhost:3000"
+      title="Log in with Google"
+    />
+    {/* <Link href={`https://auth.hackney.gov.uk/auth?redirect_uri=${redirect_uri}`} Login with google></Link> */}
+  </Layout>
+);
 
 export default Home;
