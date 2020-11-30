eba6f0cecc8a8271ca108fdb1dcb10932404e110
Jaye <joshua.hackett93@gmail.com>
2020-11-30 12:54:14 +0000

    add evidence form

---
+++ src/components/Radio.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Field as FormikField } from 'formik';
+import { Label, Hint } from 'lbh-frontend-react';
+
+const Radio = (props: Props): JSX.Element => (
+  <div className="govuk-radios__item">
+    <FormikField
+      type="radio"
+      name={props.name}
+      id={`${props.name}-${props.value}`}
+      className="govuk-radios__input"
+      value={props.value}
+    />
+    <Label
+      labelFor={`${props.name}-${props.value}`}
+      className="govuk-label govuk-radios__label"
+    >
+      {props.label}
+    </Label>
+  </div>
+);
+
+export interface Props {
+  label: string;
+  name: string;
+  value: string;
+}
+
+export default Radio;
