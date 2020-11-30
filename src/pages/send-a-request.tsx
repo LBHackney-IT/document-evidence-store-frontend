d2e36515be2a67dbb3ca9b45ad7606d5fa60a6e2
Jaye <joshua.hackett93@gmail.com>
2020-11-30 10:54:23 +0000

    refactor layout into seperate component

--- src/pages/send-a-request.tsx
+++ src/pages/send-a-request.tsx
@@ -7,28 +7,16 @@ import {
   PhaseBanner,
   Tile,
 } from 'lbh-frontend-react';
+import Layout from '../components/Layout';
 import { ReactNode } from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import fetch from 'isomorphic-unfetch';
 
-const SendARequest = (): ReactNode => {
-  return (
-    <div>
-      <Header serviceName="Document Evidence Service">
-        <title>Document Evidence Service</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Header>
-      <Container>
-        <PhaseBanner phase="ALPHA" url="form-url" />
-      </Container>
-      <Main>
-        <div className="lbh-container">
-          <Heading level={HeadingLevels.H1}>Send a request</Heading>
-        </div>
-      </Main>
-    </div>
-  );
-};
+const SendARequest = (): ReactNode => (
+  <Layout>
+    <Heading level={HeadingLevels.H1}>Send a request</Heading>
+  </Layout>
+);
 
 export default SendARequest;
