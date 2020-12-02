9285ac417006867c2e4727762a2ee99b4ea667f2
Jaye <joshua.hackett93@gmail.com>
2020-12-02 13:16:32 +0000

    add unit tests for checkbox and field

---
+++ src/components/Checkbox.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import Checkbox from './Checkbox';
+import { Formik, Form } from 'formik';
+
+test('is properly named, labelled and responds to clicks', async () => {
+  render(
+    <Formik initialValues={{ exampleName: false }} onSubmit={() => {}}>
+      <Form>
+        <Checkbox label="Example label" name="exampleName" />
+      </Form>
+    </Formik>
+  );
+
+  expect(screen.getByLabelText('Example label'));
+  expect(screen.getByRole('checkbox')).toHaveProperty('checked', false);
+  expect(screen.getByRole('checkbox')).toHaveProperty('name', 'exampleName');
+
+  fireEvent.click(screen.getByLabelText('Example label'));
+
+  await waitFor(() =>
+    expect(screen.getByRole('checkbox')).toHaveProperty('checked', true)
+  );
+});
