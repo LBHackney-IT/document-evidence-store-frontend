1e08b7ac0b2722031e698891d5f8e7b420c0a75e
Sam Garson <samtgarson@gmail.com>
2020-12-04 12:33:57 +0000

    Add cypress tests for two existing pages

---
+++ cypress/integration/view-evidence-requests.spec.ts
@@ -0,0 +1,18 @@
+describe('Can view evidence requests', () => {
+  it("has 'Please log in' heading", () => {
+    cy.visit(`http://localhost:3000`);
+    cy.get('nav').contains('Requests').click();
+
+    cy.get('h2').should('contain', 'Pending requests');
+    cy.contains('Loading').should('exist');
+
+    cy.get('tbody tr')
+      .should('have.length', 3)
+      .first()
+      .should('contain', 'Namey McName')
+      .and('contain', 'Passport')
+      .and('contain', '3 days ago');
+  });
+});
+
+export {};
