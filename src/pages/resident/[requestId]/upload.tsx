2aaa9d6e684d7366d497746745606ab6bdf6da15
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-12-08 15:29:28 +0000

    move router hook back up to page

--- src/pages/resident/[requestId]/upload.tsx
+++ src/pages/resident/[requestId]/upload.tsx
@@ -3,12 +3,15 @@ import { Heading, HeadingLevels } from 'lbh-frontend-react';
 import Layout from '../../../components/ResidentLayout';
 import { ReactNode } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import UploaderForm from '../../../components/UploaderForm';
 import { DocumentType } from '../../../domain/document-type';
 import { EvidenceRequest } from '../../../domain/evidence-request';
 import { InternalApiGateway } from '../../../gateways/internal-api';
 
 const Index = (): ReactNode => {
+  const router = useRouter();
+  const { requestId } = router.query;
   const [documentTypes, setDocumentTypes] = useState<DocumentType[]>();
   const [evidenceRequest, setEvidenceRequest] = useState<EvidenceRequest>();
 
@@ -33,6 +36,7 @@ const Index = (): ReactNode => {
             <UploaderForm
               documentTypes={documentTypes}
               evidenceRequest={evidenceRequest}
+              requestId={requestId}
             />
           ) : (
             <p>Loading...</p>
