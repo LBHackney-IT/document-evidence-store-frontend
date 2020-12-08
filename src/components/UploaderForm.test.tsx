a215da571d64e3da8cc5491fb8a4e6959d67be62
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-12-08 16:55:41 +0000

    add some test assertions

--- src/components/UploaderForm.test.tsx
+++ src/components/UploaderForm.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import UploaderForm from './UploaderForm';
 import { InternalApiGateway } from '../gateways/internal-api';
 
-test('', async () => {
+test('it renders an uploader panel and a continue button', async () => {
   const gateway = new InternalApiGateway();
   const documentTypes = await gateway.getDocumentTypes(true);
   const evidenceRequest = await gateway.getEvidenceRequest(true);
@@ -15,4 +15,8 @@ test('', async () => {
       evidenceRequest={evidenceRequest}
     />
   );
+  expect(screen.getByText('Passport')); 
+  expect(screen.getByText('A valid passport open at the photo page')); 
+  expect(screen.getByText('Browse...'));  
+  expect(screen.getByText('Continue'));
 });
