de1eee2e49e7255b4ba136ad8d1d1bbac4c20ff5
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-12-21 18:01:07 +0000

    test that uploader form validates correctly

--- src/components/UploaderForm.test.tsx
+++ src/components/UploaderForm.test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import UploaderForm from './UploaderForm';
 import singleEvidenceRequestFixture from '../../test/fixture/evidence-request-response-singular.json';
 import documentTypesFixture from '../../test/fixture/document-types-response.json';
 import { ResponseMapper } from '../boundary/response-mapper';
 
+const documentTypes = documentTypesFixture.map((dt) =>
+  ResponseMapper.mapDocumentType(dt)
+);
+const evidenceRequest = ResponseMapper.mapEvidenceRequest(
+  singleEvidenceRequestFixture
+);
+
 describe('UploaderForm', () => {
   it('renders an uploader panel and a continue button', async () => {
-    const documentTypes = documentTypesFixture.map((dt) =>
-      ResponseMapper.mapDocumentType(dt)
-    );
-    const evidenceRequest = ResponseMapper.mapEvidenceRequest(
-      singleEvidenceRequestFixture
-    );
-
     render(
       <UploaderForm
         requestId="foo"
@@ -23,7 +23,20 @@ describe('UploaderForm', () => {
     );
     expect(screen.getByText('Passport'));
     expect(screen.getByText('A valid passport open at the photo page'));
-    // expect(screen.getByText('Browse...'));
     expect(screen.getByText('Continue'));
+  });
+
+  it('validates files are attached', async () => {
+    render(
+      <UploaderForm
+        requestId="foo"
+        documentTypes={documentTypes}
+        evidenceRequest={evidenceRequest}
+      />
+    );
+    fireEvent.click(screen.getByText('Continue'));
+    await waitFor(() => {
+      expect(screen.getByText('Please select a file'));
+    });
   });
 });
