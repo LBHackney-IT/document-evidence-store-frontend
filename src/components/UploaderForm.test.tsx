97ea3236275253dde0772a79f950ebbbd0cbe3de
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-12-21 16:59:09 +0000

    change test style

--- src/components/UploaderForm.test.tsx
+++ src/components/UploaderForm.test.tsx
@@ -3,20 +3,22 @@ import { render, screen } from '@testing-library/react';
 import UploaderForm from './UploaderForm';
 import { InternalApiGateway } from '../gateways/internal-api';
 
-test('it renders an uploader panel and a continue button', async () => {
-  const gateway = new InternalApiGateway();
-  const documentTypes = await gateway.getDocumentTypes(true);
-  const evidenceRequest = await gateway.getEvidenceRequest(true);
+describe('UploaderForm', () => {
+  it('renders an uploader panel and a continue button', async () => {
+    const gateway = new InternalApiGateway();
+    const documentTypes = await gateway.getDocumentTypes(true);
+    const evidenceRequest = await gateway.getEvidenceRequest(true);
 
-  render(
-    <UploaderForm
-      requestId="foo"
-      documentTypes={documentTypes}
-      evidenceRequest={evidenceRequest}
-    />
-  );
-  expect(screen.getByText('Passport'));
-  expect(screen.getByText('A valid passport open at the photo page'));
-  expect(screen.getByText('Browse...'));
-  expect(screen.getByText('Continue'));
+    render(
+      <UploaderForm
+        requestId="foo"
+        documentTypes={documentTypes}
+        evidenceRequest={evidenceRequest}
+      />
+    );
+    expect(screen.getByText('Passport'));
+    expect(screen.getByText('A valid passport open at the photo page'));
+    expect(screen.getByText('Browse...'));
+    expect(screen.getByText('Continue'));
+  });
 });
