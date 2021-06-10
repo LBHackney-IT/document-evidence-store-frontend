26fce2a48f7bdb70ef6d6d061b59a8bb0d708d18
Neil Mendum <nmendum@gmail.com>
2021-06-10 13:37:11 +0100

    DES-308: make call to documents endpoint

--- test/mocks/fixtures/get-document.js
+++ test/mocks/fixtures/get-document.js
@@ -1,10 +1,10 @@
 const getDocument = {
   id: 'get-document',
-  url: '/api/v1/claims/:id/download',
+  url: '/api/v1/documents/:id',
   method: 'GET',
   response: {
     status: 200,
-    body: new ArrayBuffer(20),
+    body: 'documentAsByte64',
   },
 };
 
