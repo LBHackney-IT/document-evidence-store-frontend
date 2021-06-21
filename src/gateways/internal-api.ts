0f424cd789f34ae845b081ea44f43f328ddef53b
bogdan-zaharia <54269120+bogdan-zaharia@users.noreply.github.com>
2021-06-21 09:42:52 +0100

    Show the error message received from evidence-api on the request form (#136)
    * Show the error message received from evidence-api on the request form
    
    * Add back error logging

--- src/gateways/internal-api.ts
+++ src/gateways/internal-api.ts
@@ -88,7 +88,10 @@ export class InternalApiGateway {
 
       return ResponseMapper.mapEvidenceRequest(data);
     } catch (err) {
-      console.error(err);
+      if (err.response) {
+        console.error(err);
+        throw err.response.data;
+      }
       throw new InternalServerError('Internal server error');
     }
   }
