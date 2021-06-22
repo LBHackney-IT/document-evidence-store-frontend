22af34fc62d99611ff1f43b3b56b5e446b311d75
Bogdan <bogdan@madetech.com>
2021-06-22 16:13:02 +0100

    Fix unit tests

--- src/components/EvidenceTile.test.tsx
+++ src/components/EvidenceTile.test.tsx
@@ -21,8 +21,6 @@ describe('EvidenceTile', () => {
     expect(screen.getByText('1 day ago'));
     expect(screen.getByText('1.0 KB'));
     expect(screen.getByText('PDF'));
-    expect(screen.getByText('Accept'));
-    expect(screen.getByText('Request new file'));
   });
 
   it('renders without actions if document has already been reviewed', async () => {
