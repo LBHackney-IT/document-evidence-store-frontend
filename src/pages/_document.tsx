7a66ccd0cb02755d281892622afeb9abe151e174
Neil Mendum <nmendum@gmail.com>
2021-09-13 12:00:05 +0100

    DES-342: Update language identifier to UK English

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class CustomDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html lang="en-gb">
         <Head />
         <body>
           <Main />
