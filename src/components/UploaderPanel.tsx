2aa2c0f504962bf986a41592f8a575333293e15d
Bella <arabella.cockrell@gmail.com>
2022-02-11 11:50:49 +0000

    Add comments for file extensions and remove octet-stream due to vulnerable content-type

--- src/components/UploaderPanel.tsx
+++ src/components/UploaderPanel.tsx
@@ -10,23 +10,22 @@ const classNameFromProps = (props: Props) => {
 
 const acceptedMimeTypes = (): string => {
   const acceptedMimeTypes = [
-    'application/msword',
-    'application/octet-stream',
-    'application/pdf',
-    'application/vnd.apple.numbers',
-    'application/vnd.apple.pages',
-    'application/vnd.ms-excel',
-    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'image/bmp',
-    'image/gif',
-    'image/heic',
-    'image/jpeg',
-    'image/png',
-    'text/plain',
-    //'video/3gpp',
-    //'video/mp4',
-    //'video/quicktime',
+    'application/msword', //.doc
+    'application/pdf', //.pdf
+    'application/vnd.apple.numbers', //.numbers
+    'application/vnd.apple.pages', //.pages
+    'application/vnd.ms-excel', //.xls
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', //.xlsx
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', //.docx
+    'image/bmp', //.bmp
+    'image/gif', //.gif
+    'image/heic', //.heic
+    'image/jpeg', //.jpeg or .jpg
+    'image/png', //.png
+    'text/plain', //.txt
+    //'video/3gpp', //.3gpp or .3gp
+    //'video/mp4', //.mp4
+    //'video/quicktime', //.mov or .qt
   ];
 
   return acceptedMimeTypes.join(',');
