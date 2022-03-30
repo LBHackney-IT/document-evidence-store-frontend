e0ccf529277fab97449030257a1acd7f1f10112c
bogdan-zaharia <54269120+bogdan-zaharia@users.noreply.github.com>
2022-03-30 11:28:41 +0100

    Remove old download path (#213)

--- src/gateways/documents-api.test.ts
+++ src/gateways/documents-api.test.ts
@@ -11,40 +11,6 @@ describe('Documents api gateway', () => {
     client: (client as unknown) as AxiosInstance,
   });
 
-  describe('GET request to /documents', () => {
-    const documentId = '456';
-    const expectedData = 'response to GET';
-    const expectedStatus = 200;
-    const expectedResponse = { data: expectedData, status: expectedStatus };
-
-    beforeEach(() => {
-      client.get.mockResolvedValue({ data: expectedResponse });
-    });
-
-    it('the request returns the correct response', async () => {
-      const response = await gateway.getDocument(documentId);
-      expect(response).toEqual(expectedResponse);
-      expect(client.get).toHaveBeenCalledWith(
-        `/api/v1/documents/${documentId}`,
-        {
-          headers: {
-            Authorization: process.env.DOCUMENTS_API_GET_DOCUMENTS_TOKEN,
-          },
-        }
-      );
-    });
-
-    describe('when there is an error', () => {
-      it('returns internal server error', async () => {
-        client.get.mockRejectedValue(new Error('Network error'));
-        const functionCall = () => gateway.getDocument(documentId);
-        expect(functionCall).rejects.toEqual(
-          new InternalServerError('Internal server error')
-        );
-      });
-    });
-  });
-
   describe('GET request to /claims/{claimId}/download_links', () => {
     const claimId = '123-abc-claimId';
     const expectedData = 'www.downloadlinkfordocument.com';
