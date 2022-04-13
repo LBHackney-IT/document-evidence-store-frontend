e4d3f9696bef68c40329a9437bd15b706b4b2fb1
Bella <arabella.cockrell@gmail.com>
2022-04-13 16:51:56 +0100

    Remove prettyDOM and test

--- src/components/TextAreaWithCharacterCount.test.tsx
+++ src/components/TextAreaWithCharacterCount.test.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  prettyDOM,
-} from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { TextAreaWithCharacterCount } from './TextAreaWithCharacterCount';
 import { Formik, Form } from 'formik';
 
@@ -13,7 +7,7 @@ const promise = Promise.resolve();
 const mockHandler = jest.fn(() => promise);
 
 describe('TextAreaWithCharacterCount', () => {
-  it('renders a text area with the correct character count', async () => {
+  it('renders a text area with the correct character count and updates when text area value changes', async () => {
     const initialValues = {
       noteToResident: '',
     };
@@ -43,28 +37,5 @@ describe('TextAreaWithCharacterCount', () => {
     await waitFor(() => {
       expect(screen.getByText('You have 1 character remaining')).toBeVisible();
     });
-  });
-
-  it('renders a text area with a different character count', async () => {
-    const initialValues = {
-      noteToResident: 'hello',
-    };
-    render(
-      <Formik onSubmit={mockHandler} initialValues={initialValues}>
-        <Form>
-          <TextAreaWithCharacterCount
-            maxCharacterLength={6}
-            name="noteToResident"
-            id="noteToResident"
-            dataTestId="testid"
-            rows={5}
-          />
-        </Form>
-      </Formik>
-    );
-
-    expect(screen.getByText('You have 1 character remaining')).toBeVisible();
-    console.log(prettyDOM());
-    expect(screen.getByRole('textbox')).toHaveProperty('value', 'hello');
   });
 });
