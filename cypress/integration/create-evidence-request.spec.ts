142e96ce4cab6c3fe1f62d4717f641630ace74fd
Bella <arabella.cockrell@gmail.com>
2022-04-14 11:17:02 +0100

    Add e2e tests

--- cypress/integration/create-evidence-request.spec.ts
+++ cypress/integration/create-evidence-request.spec.ts
@@ -46,6 +46,7 @@ describe('Create evidence requests', () => {
       cy.get('li').contains('frodo@bagend.com');
       cy.get('li').contains('+447123456780');
       cy.get('li').contains('Proof of ID');
+      cy.get('p').contains('Not all those who wander are lost');
 
       cy.get('button').contains('Confirm').click();
     });
@@ -53,6 +54,70 @@ describe('Create evidence requests', () => {
     cy.wait('@postEvidenceRequests');
 
     cy.get('body').contains('Thanks!');
+  });
+
+  describe('When a user has not entered in text for note to resident', () => {
+    it('they cannot click continue', () => {
+      cy.visit(`http://localhost:3000/teams/2/dashboard/requests/new/1`);
+
+      cy.get('label').contains('Name').next('input').type('Frodo Baggins');
+      cy.get('label').contains('Email').next('input').type('frodo@bagend.com');
+
+      cy.get('button').contains('Continue').click();
+      cy.get('label').contains('Proof of ID').click();
+      cy.get('button').contains('Continue').click();
+
+      cy.get('button').should('be.disabled');
+    });
+
+    it('they click skip and continue, and no note is on confirmation page', () => {
+      cy.visit(`http://localhost:3000/teams/2/dashboard/requests/new/1`);
+
+      cy.get('label').contains('Name').next('input').type('Frodo Baggins');
+      cy.get('label').contains('Email').next('input').type('frodo@bagend.com');
+
+      cy.get('button').contains('Continue').click();
+      cy.get('label').contains('Proof of ID').click();
+      cy.get('button').contains('Continue').click();
+
+      cy.get('button').contains('Skip and Continue').click();
+
+      cy.get('[role=dialog]').within(() => {
+        cy.get('li').contains('Frodo Baggins');
+        cy.get('li').contains('frodo@bagend.com');
+        cy.get('li').contains('Proof of ID');
+        cy.get('.govuk-inset-text').should('not.exist');
+      });
+    });
+  });
+
+  describe('When a user has entered in whitespace for note to resident', () => {
+    it('no note is on the confirmation page', () => {
+      cy.visit(`http://localhost:3000/teams/2/dashboard/requests/new/1`);
+
+      cy.get('label').contains('Name').next('input').type('Frodo Baggins');
+      cy.get('label').contains('Email').next('input').type('frodo@bagend.com');
+
+      cy.get('button').contains('Continue').click();
+      cy.get('label').contains('Proof of ID').click();
+      cy.get('button').contains('Continue').click();
+
+      cy.get('[data-testid="textarea"]').type('      ');
+      cy.get('button').contains('Continue').click();
+
+      cy.get('[role=dialog]').within(() => {
+        cy.get('li').contains('Frodo Baggins');
+        cy.get('li').contains('frodo@bagend.com');
+        cy.get('li').contains('Proof of ID');
+        cy.get('.govuk-inset-text').should('not.exist');
+
+        cy.get('button').contains('Confirm').click();
+      });
+
+      cy.wait('@postEvidenceRequests');
+
+      cy.get('body').contains('Thanks!');
+    });
   });
 });
 
