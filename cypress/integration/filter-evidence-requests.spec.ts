a15a12653946128c6bc11c2f2b12277a5b7ab1ac
Bella <arabella.cockrell@gmail.com>
2022-06-07 09:10:44 +0100

    Add e2e test for filtered evidence requests

--- cypress/integration/filter-evidence-requests.spec.ts
+++ cypress/integration/filter-evidence-requests.spec.ts
@@ -13,24 +13,23 @@ describe('Can filter evidence requests', () => {
   });
 
   it('User can filter evidence requests', () => {
-    // cy.intercept('GET', '?team=Development+Housing+Team&state=1', {
-    //     fixture: 'evidence_requests/get-to-review',
-    //   }).as('filteredEvidenceRequests');
+    cy.intercept('GET', '?team=Development+Housing+Team&state=1', {
+      fixture: 'evidence_requests/get-to-review',
+    }).as('filteredEvidenceRequests');
 
     cy.get('label')
       .contains('Display only residents with documents pending review')
       .click();
 
-    //   cy.wait('@filteredEvidenceRequests')
-
-    // cy.wait('@filteredEvidenceRequests').its('response.statusCode').should('eq', 200)
-
-    //   .then((interception) => {
-    //       assert.isNotNull(interception.response?.body, 'this is the response')
-    //   })
+    cy.wait('@filteredEvidenceRequests').then((interception) => {
+      assert.include(
+        interception.request?.url,
+        '?team=Development+Housing+Team&state=1'
+      );
+      assert.equal(interception.response?.statusCode, 200);
+      assert.isNotNull(interception.response?.body);
+    });
 
     cy.get('tbody tr').should('have.length', 1);
   });
 });
-
-export {};
