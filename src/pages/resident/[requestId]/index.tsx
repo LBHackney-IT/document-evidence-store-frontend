553f8a697f2875eb63fb5f79d279f582f997b50d
Bella <arabella.cockrell@gmail.com>
2022-07-15 16:28:19 +0200

    Update landing page and tests

--- src/pages/resident/[requestId]/index.tsx
+++ src/pages/resident/[requestId]/index.tsx
@@ -19,24 +19,12 @@ const Index: NextPage<IndexProps> = ({ requestId, team }) => {
     <Layout feedbackUrl={process.env.FEEDBACK_FORM_RESIDENT_URL as string}>
       <Head>
         <title>
-          You’ll need to photograph your documents | Document Evidence Service |
-          Hackney Council
+          Please have your documents ready | Document Evidence Service | Hackney
+          Council
         </title>
       </Head>
       <InterruptionCard>
-        <h1 className="lbh-heading-h1">
-          You’ll need to photograph your documents
-        </h1>
-        <p className="lbh-body">
-          You can use your smartphone camera. First, make sure you’re in a
-          well-lit place.
-        </p>
-        <p className="lbh-body">
-          Lie the document flat and try not to cover any part of it.
-        </p>
-        <p className="lbh-body">
-          Make sure the whole document is in the frame.
-        </p>
+        <h1 className="lbh-heading-h1">Please have your documents ready</h1>
         <p className="lbh-body">{team.landingMessage}</p>
         <Link href={`/resident/${requestId}/upload`}>
           <a className="govuk-button lbh-button">Continue</a>
