a2cb4fef96593fe4f868303f79291dba40c71439
aburathoki <aburathoki@gmail.com>
2022-08-01 16:29:59 +0100

    Doc 1011 (#238)
    * fix flaky tests
    
    * increase character for bespoke note
    
    Co-authored-by: asha burathoki <asha.burathoki@madetech.com>

--- src/components/NewRequestFormStep3.tsx
+++ src/components/NewRequestFormStep3.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as Yup from 'yup';
 import { TextAreaWithCharacterCount } from './TextAreaWithCharacterCount';
 
-const maxCharacterLength = 400;
+const maxCharacterLength = 5000;
 
 export const schemaNewRequestFormStep3 = Yup.object().shape({
   noteToResident: Yup.string().max(maxCharacterLength),
