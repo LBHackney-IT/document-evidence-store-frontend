44204d80a27e531fbd128a04996a6183fc58e9c6
asha burathoki <aburathoki@gmail.com>
2022-08-25 11:50:39 +0100

    'Re-styled Resident Page Table'

--- src/components/ResidentPageTable.tsx
+++ src/components/ResidentPageTable.tsx
@@ -1,29 +1,37 @@
 import { Resident } from 'src/domain/resident';
 import React, { FunctionComponent } from 'react';
 
-const ResidentPageTable: FunctionComponent<Props> = ({ resident }) => (
-  <table className="govuk-table lbh-table">
-    <tbody className="govuk-table__body">
-      <tr className="govuk-table__row">
-        <td className="govuk-table__cell">Name</td>
-        <td className="govuk-table__cell ">{resident.name}</td>
-      </tr>
-      <tr className="govuk-table__row">
-        <td className="govuk-table__cell">Mobile Number</td>
-        {resident.phoneNumber.length ? (
-          <td className="govuk-table__cell">{resident.phoneNumber}</td>
-        ) : (
-          <td className="govuk-table__cell">-</td>
-        )}
-      </tr>
-      <tr className="govuk-table__row">
-        <td className="govuk-table__cell">Email</td>
-        <td className="govuk-table__cell">{resident.email}</td>
-      </tr>
-    </tbody>
-  </table>
-);
-
+const ResidentPageTable: FunctionComponent<Props> = (props) => {
+  const { resident } = props;
+  return (
+    <table className="govuk-table lbh-table">
+      <tbody className="govuk-table__body">
+        <tr className="govuk-table__row">
+          <th scope="row" className="govuk-table__header">
+            Name
+          </th>
+          <td className="govuk-table__cell ">{resident.name}</td>
+        </tr>
+        <tr className="govuk-table__row">
+          <th scope="row" className="govuk-table__header">
+            Mobile Number
+          </th>
+          {resident.phoneNumber.length ? (
+            <td className="govuk-table__cell">{resident.phoneNumber}</td>
+          ) : (
+            <td className="govuk-table__cell">-</td>
+          )}
+        </tr>
+        <tr className="govuk-table__row">
+          <th scope="row" className="govuk-table__header">
+            Email
+          </th>
+          <td className="govuk-table__cell">{resident.email}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 export interface Props {
   resident: Resident;
 }
