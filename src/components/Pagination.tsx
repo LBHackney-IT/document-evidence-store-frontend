b45270d77a15de461592630ca52f7c30e1ba6560
Charli Wild <charli.wild@charlis-mbp.lan>
2022-11-09 14:44:43 +0000

    added function to generate pagination links

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,48 +1,72 @@
-import React, { FunctionComponent } from 'react';
-//import { number } from 'yup'; commenting to push up with no errors
+import React, { FunctionComponent, useState, useEffect } from 'react';
 
-// const numberOfPages = (total: number, pageSize: number): number => {
-//   return Math.ceil(total / pageSize);
-// };
+const [currentPage, setCurrentPage] = useState(1);
+const [totalPages, setTotalPages] = useState(0);
 
-export const Pagination: FunctionComponent<Props> = (props: Props) => (
-  <nav className="lbh-pagination">
-    <ul className="lbh-pagination">
-      <li className="lbh-pagination__item">
-        {
-          //don't display for first page
-        }
-        <a
-          className="lbh-pagination__link"
-          href="#"
-          aria-label="Previous page"
-          onClick={() => props.onPageChange(props.currentPageNumber - 1)}
-        >
-          <span aria-hidden="true" role="presentation">
-            &laquo;
-          </span>
-          Previous
-        </a>
-      </li>
-      <li className="lbh-pagination__item">
-        {
-          //don't display for last page
-        }
-        <a
-          className="lbh-pagination__link"
-          href="#"
-          aria-label="Next page"
-          onClick={() => props.onPageChange(props.currentPageNumber - 1)}
-        >
-          Next
-          <span aria-hidden="true" role="presentation">
-            &raquo;
-          </span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+export const Pagination: FunctionComponent<Props> = (props: Props) => {
+  useEffect(() => {
+    setTotalPages(Math.ceil(props.total / props.pageSize));
+  }, []);
+
+  const handlePaginate = (nextPageNumber: number) => {
+    setCurrentPage(nextPageNumber);
+    props.onPageChange(nextPageNumber);
+  };
+
+  const generatePaginationLinks = () => {
+    for (let page = 1; page <= props.total; page++) {
+      return (
+        <li className="govuk-pagination__item">
+          <a
+            className="govuk-link govuk-pagination__link"
+            href="#"
+            aria-label={`page ${page}`}
+          >
+            {page}
+          </a>
+        </li>
+      );
+    }
+  };
+
+  return (
+    <nav className="lbh-pagination">
+      <ul className="lbh-pagination">
+        {currentPage != 1 && (
+          <li className="lbh-pagination__item">
+            <a
+              className="lbh-pagination__link"
+              href="#"
+              aria-label="Previous page"
+              onClick={() => handlePaginate(props.currentPageNumber - 1)}
+            >
+              <span aria-hidden="true" role="presentation">
+                &laquo;
+              </span>
+              Previous
+            </a>
+          </li>
+        )}
+        <>{generatePaginationLinks()}</>
+        {currentPage != totalPages && (
+          <li className="lbh-pagination__item">
+            <a
+              className="lbh-pagination__link"
+              href="#"
+              aria-label="Next page"
+              onClick={() => handlePaginate(props.currentPageNumber + 1)}
+            >
+              Next
+              <span aria-hidden="true" role="presentation">
+                &raquo;
+              </span>
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 export interface Props {
   currentPageNumber: number;
