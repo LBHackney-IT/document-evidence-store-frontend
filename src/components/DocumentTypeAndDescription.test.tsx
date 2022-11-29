d5b052a775aca1a5a3ee81d5b3d24939e121a2b3
Charli Wild <charli.wild@charlis-mbp.lan>
2022-11-29 11:29:48 +0000

    added error test for new component

--- src/components/DocumentTypeAndDescription.test.tsx
+++ src/components/DocumentTypeAndDescription.test.tsx
@@ -2,10 +2,11 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import DocumentTypeAndDescription from './DocumentTypeAndDescription';
 import { DocumentType } from '../domain/document-type';
+import { UploaderPanelError } from './StaffUploaderForm';
 
 describe('DocumentTypeAndDescription', () => {
+  const testDocTypes: DocumentType[] = [];
   it('renders the expected data', () => {
-    const testDocTypes: DocumentType[] = [];
     render(
       <DocumentTypeAndDescription
         name={'Test document'}
@@ -20,5 +21,23 @@ describe('DocumentTypeAndDescription', () => {
 
     expect(screen.getByLabelText('Document type')).toHaveValue();
     expect(descriptionField).toHaveTextContent('test description');
+  });
+  it('renders an expected error', () => {
+    var error: UploaderPanelError = {
+      staffSelectedDocumentType: ' test',
+      description: 'this is a test error',
+      files: [],
+    };
+    render(
+      <DocumentTypeAndDescription
+        name={'Test document'}
+        documentTypes={testDocTypes}
+        error={error}
+      />
+    );
+    expect(screen.getByLabelText('Description')).toHaveProperty(
+      'error',
+      'this is a test error'
+    );
   });
 });
