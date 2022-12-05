53f517cffa986500da109219520d61613f3413f4
Charli Wild <charli.wild@charlis-mbp.lan>
2022-12-05 10:14:08 +0000

    skeleton createResident form

---
+++ src/components/CreateResidentForm.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Field from './Field';
+import { useFormikContext } from 'formik';
+//import { EvidenceRequestForm } from 'src/gateways/internal-api';
+import * as Yup from 'yup';
+
+interface placeHolderResident {
+  name: string;
+  email: string;
+  phoneNumber: string;
+}
+
+const emailOrPhoneNumberMessage =
+  'Please provide either an email or a phone number';
+
+export const schemaCreateResidentForm = Yup.object().shape(
+  {
+    resident: Yup.object().shape(
+      {
+        name: Yup.string().required("Please enter the resident's name"),
+        email: Yup.string().when(['phoneNumber'], {
+          is: (phoneNumber) => !phoneNumber,
+          then: Yup.string()
+            .required(emailOrPhoneNumberMessage)
+            .email('Please provide a valid email address'),
+        }),
+        phoneNumber: Yup.string().when(['email'], {
+          is: (email) => !email,
+          then: Yup.string()
+            .required(emailOrPhoneNumberMessage)
+            .matches(/^\+?[\d]{6,14}$/, 'Please provide a valid phone number'),
+        }),
+      },
+      [['email', 'phoneNumber']]
+    ),
+  },
+  [['emailCheckbox', 'phoneNumberCheckbox']]
+);
+
+const CreateResidentForm = (): JSX.Element => {
+  const { errors, touched } = useFormikContext<placeHolderResident>(); //create resident request //replace values
+
+  return (
+    <>
+      <h1 className="lbh-heading-h2">Create A New Resident</h1>
+
+      <Field
+        label="Name"
+        name="name"
+        error={touched.name ? errors.name : null}
+      />
+      <Field
+        label="Email"
+        name="email"
+        error={touched.email ? errors.email : null}
+      />
+      <Field
+        label="Mobile phone number"
+        name="phoneNumber"
+        error={touched.phoneNumber ? errors.phoneNumber : null}
+      />
+    </>
+  );
+};
+
+export default CreateResidentForm;
