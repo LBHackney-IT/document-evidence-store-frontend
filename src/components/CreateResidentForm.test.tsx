b937a9ec1956ce2ff6fb08fd9ab6b0d89fe080ab
Charli Wild <charli.wild@charlis-mbp.lan>
2022-12-08 14:39:26 +0000

    fixed basic test for new component

--- src/components/CreateResidentForm.test.tsx
+++ src/components/CreateResidentForm.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import CreateResidentForm from './CreateResidentForm';
-import { schemaCreateResidentForm } from './CreateResidentForm';
+import { createResidentSchema } from './CreateResidentForm';
 
 import { Formik, Form } from 'formik';
 
@@ -14,18 +14,18 @@ describe('CreateResidentForm', () => {
           email: '',
           phoneNumber: '',
         }}
-        validationSchema={schemaCreateResidentForm}
+        validationSchema={createResidentSchema}
         onSubmit={jest.fn()}
       >
         <Form>
-          <CreateResidentForm />
+          <CreateResidentForm onSuccess={jest.fn()} />
           <button type="submit">Continue</button>
         </Form>
       </Formik>
     );
 
     expect(getByLabelText('Name')).toBeInTheDocument();
-    expect(getByLabelText('Email')).toBeInTheDocument();
-    expect(getByLabelText('Mobile phone number')).toBeInTheDocument();
+    expect(getByLabelText('Email Address')).toBeInTheDocument();
+    expect(getByLabelText('Phone Number')).toBeInTheDocument();
   });
 });
