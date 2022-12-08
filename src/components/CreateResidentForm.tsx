b4a6e30158affcb64e5bd742006ca01858bddfee
Charli Wild <charli.wild@charlis-mbp.lan>
2022-12-08 11:48:46 +0000

    added happy path

--- src/components/CreateResidentForm.tsx
+++ src/components/CreateResidentForm.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Field from './Field';
-import { useFormikContext, Formik } from 'formik';
+import { Formik, Form } from 'formik';
 import { CreateResidentRequest } from 'src/gateways/internal-api';
 import * as Yup from 'yup';
-import { Resident } from 'src/domain/resident';
 
 export const emailOrPhoneNumberMessage =
   'Please provide either an email or a phone number';
@@ -30,41 +29,52 @@ export const schemaCreateResidentForm = Yup.object().shape({
 });
 
 const CreateResidentForm = (props: Props): JSX.Element => {
-  const { errors, touched } = useFormikContext<CreateResidentRequest>();
-
   return (
     <>
       <h1 className="lbh-heading-h2">Create A New Resident</h1>
       <Formik
-        initialValues={{}}
-        onSubmit={() => {
-          props.createResident;
+        initialValues={{
+          name: '',
+          email: '',
+          phoneNumber: '',
+        }}
+        onSubmit={(values: CreateResidentRequest) => {
+          console.log('calling onSubmit with these values: ' + values);
+          props.createResident(values);
         }}
         validationScheme={schemaCreateResidentForm}
       >
-        <Field
-          label="Name"
-          name="name"
-          error={touched.name ? errors.name : null}
-        />
-        <Field
-          label="Email"
-          name="email"
-          error={touched.email ? errors.email : null}
-        />
-        <Field
-          label="Mobile phone number"
-          name="phoneNumber"
-          error={touched.phoneNumber ? errors.phoneNumber : null}
-        />
-        <button className="govuk-button lbh-button">Search</button>
+        {({ errors, touched }) => (
+          <>
+            <Form>
+              <Field
+                label="Name"
+                name="name"
+                error={touched.name ? errors.name : null}
+              />
+              <Field
+                label="Email"
+                name="email"
+                error={touched.email ? errors.email : null}
+              />
+              <Field
+                label="Mobile phone number"
+                name="phoneNumber"
+                error={touched.phoneNumber ? errors.phoneNumber : null}
+              />
+              <button className="govuk-button lbh-button" type="submit">
+                Create
+              </button>
+            </Form>
+          </>
+        )}
       </Formik>
     </>
   );
 };
 
 interface Props {
-  createResident(resident: CreateResidentRequest): Promise<Resident>;
+  createResident(resident: CreateResidentRequest): void;
 }
 
 export default CreateResidentForm;
--- src/pages/teams/[teamId]/dashboard/residents/create/index.tsx
+++ src/pages/teams/[teamId]/dashboard/residents/create/index.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import CreateResidentForm from 'src/components/CreateResidentForm';
-import { CreateResidentRequest } from 'src/gateways/internal-api';
-import { Resident } from 'src/domain/resident';
-import { EvidenceApiGateway } from 'src/gateways/evidence-api';
+import {
+  CreateResidentRequest,
+  InternalApiGateway,
+} from 'src/gateways/internal-api';
 import { Constants } from 'src/helpers/Constants';
+import Layout from 'src/components/DashboardLayout';
+import Head from 'next/head';
 
 const CreateResidentPage: React.FC = () => {
-  return <CreateResidentForm createResident={createResident} />;
-};
+  const router = useRouter();
+  const { teamId } = router.query;
 
-const createResident = async (
-  resident: CreateResidentRequest
-): Promise<Resident> => {
-  const gateway = new EvidenceApiGateway();
-  //happy path
-  const { name, email, phoneNumber } = resident;
-  const newResident = await gateway.createResident(
-    name,
-    Constants.DUMMY_EMAIL,
-    email,
-    phoneNumber
+  const createResident = async (resident: CreateResidentRequest) => {
+    const gateway = new InternalApiGateway();
+    //happy path
+    const newResident = await gateway.createResident(
+      Constants.DUMMY_EMAIL,
+      resident
+    );
+
+    router.push(`/teams/${teamId}/dashboard/residents/${newResident.id}`);
+  };
+
+  return (
+    <Layout teamId={teamId?.toString() || ''} feedbackUrl={'test'}>
+      <Head>
+        <title>
+          Create A Resident | Document Evidence Service | Hackney Council
+        </title>
+      </Head>
+      <CreateResidentForm createResident={createResident} />;
+    </Layout>
   );
-  console.log(`new resident created: ${JSON.stringify(newResident)}`);
-  return newResident;
-
-  /* routing -  return {
-      redirect: {
-        destination: `/teams/${userTeams[0].id}/dashboard`,
-        permanent: false,
-      },
-    };  - can get query from url to populate team */
-
-  //add error handling
 };
 
 export default CreateResidentPage;
