a00319e9d2e0809fe45f454fce5bdc796f47b9e7
Charli Wild <charli.wild@charlis-mbp.lan>
2022-12-13 11:55:25 +0000

    added happy path cypress test

--- cypress/integration/create-a-resident.spec.ts
+++ cypress/integration/create-a-resident.spec.ts
@@ -2,12 +2,24 @@ describe('Can add a resident to the DES database', () => {
   beforeEach(() => {
     cy.login();
 
+    cy.intercept('/api/residents', {
+      fixture: 'residents/create',
+    });
+
     cy.visit(`http://localhost:3000/teams/2/dashboard/residents/create`);
     cy.injectAxe();
   });
 
   it('Has no detectable accessibility issues', () => {
     cy.checkA11y();
+  });
+
+  it('User can add a resident', () => {
+    cy.get('#name').type('Test Resident');
+    cy.get('#email').type('test@test.com');
+    cy.get('#phoneNumber').type('1-800-543-2342');
+
+    cy.get('.govuk-button').click();
   });
 });
 
