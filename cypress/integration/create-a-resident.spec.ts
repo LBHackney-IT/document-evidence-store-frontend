9d40fad6c80725404b9896c047f73433387ff706
Charli Wild <charli.wild@charlis-mbp.lan>
2022-12-13 14:48:23 +0000

    create a resident spec

--- cypress/integration/create-a-resident.spec.ts
+++ cypress/integration/create-a-resident.spec.ts
@@ -2,9 +2,9 @@ describe('Can add a resident to the DES database', () => {
   beforeEach(() => {
     cy.login();
 
-    cy.intercept('/api/residents', {
+    cy.intercept('POST', '/api/residents', {
       fixture: 'residents/create',
-    });
+    }).as('createResident');
 
     cy.visit(`http://localhost:3000/teams/2/dashboard/residents/create`);
     cy.injectAxe();
@@ -20,6 +20,29 @@ describe('Can add a resident to the DES database', () => {
     cy.get('#phoneNumber').type('1-800-543-2342');
 
     cy.get('.govuk-button').click();
+  });
+
+  context('when resident already exists', () => {
+    beforeEach(() => {
+      cy.intercept('POST', '/api/residents', (req) => {
+        req.reply((res) => {
+          res.send(400, 'The resident already exists in the system');
+        });
+      });
+    });
+
+    it('shows an error message', () => {
+      cy.get('#name').type('Test Resident');
+      cy.get('#email').type('test@test.com');
+      cy.get('#phoneNumber').type('1-800-543-2342');
+
+      cy.get('.govuk-button').click();
+
+      cy.get('span').should(
+        'contain',
+        'There was an error. Please try again later'
+      );
+    });
   });
 });
 
