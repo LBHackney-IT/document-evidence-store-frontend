28145e2ba0d72c8e9d6220a0504d7c8cc594160d
Charli Wild <charli.wild@charlis-mbp.lan>
2022-12-14 10:39:36 +0000

    update resident search

--- src/components/ResidentSearchForm.tsx
+++ src/components/ResidentSearchForm.tsx
@@ -1,37 +1,46 @@
 import React, { useState } from 'react';
 import styles from '../styles/ResidentSearch.module.scss';
+import NavLink from './NavLink';
 
 const ResidentSearchForm = (props: Props): JSX.Element => {
   const [searchQuery, setSearchQuery] = useState('');
 
   return (
-    <form
-      onSubmit={(event) => {
-        if (searchQuery.trim() != '') {
-          props.handleSearch(searchQuery);
-        }
-        event.preventDefault();
-      }}
-      className={styles.form}
-    >
-      <label className={styles.label} htmlFor="search-query">
-        Search query
-      </label>
-      <input
-        name="search-query"
-        id="search-query"
-        className="govuk-input lbh-input"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search by name or contact detail"
-      />
-      <button className="govuk-button lbh-button">Search</button>
-    </form>
+    <div>
+      <form
+        onSubmit={(event) => {
+          if (searchQuery.trim() != '') {
+            props.handleSearch(searchQuery);
+          }
+          event.preventDefault();
+        }}
+        className={styles.form}
+      >
+        <label className={styles.label} htmlFor="search-query">
+          Search query
+        </label>
+        <input
+          name="search-query"
+          id="search-query"
+          className={`govuk-input lbh-input ${styles.inputForm}`}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search by name or contact detail"
+        />
+        <button className={`govuk-button lbh-button ${styles.searchButton}`}>
+          Search
+        </button>
+        <NavLink href={`/teams/${props.teamId}/dashboard/residents/create`}>
+          Create new
+        </NavLink>
+      </form>
+    </div>
   );
 };
 
 interface Props {
   handleSearch(searchQuery: string): void;
+  teamId: string;
 }
 
 export default ResidentSearchForm;
