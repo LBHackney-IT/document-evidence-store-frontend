e4b01ac7b7daa64803ff772f295d51b98b51ff31
Charli Wild <charli.wild@charlis-mbp.lan>
2023-02-03 14:00:49 +0000

    center loading state

--- src/components/ResidentSummaryTable.tsx
+++ src/components/ResidentSummaryTable.tsx
@@ -25,7 +25,7 @@ export const ResidentSummaryTable: FunctionComponent<Props> = ({
 
   return loading ? (
     <div className={styles.spinner}>
-      <svg viewBox="0 0 42 42" stroke="#00703c" width="50" height="50">
+      <svg viewBox="0 0 42 42" stroke="#00703c" width="75" height="75">
         <g fill="none" fill-rule="evenodd">
           <g transform="translate(3 3)" stroke-width="5">
             <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
