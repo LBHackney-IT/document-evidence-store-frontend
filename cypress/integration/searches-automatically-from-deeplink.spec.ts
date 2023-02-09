e0d8ef9078083531269f64950f995ca3ca2d558e
Charli Wild <charli.wild@charlis-mbp.lan>
2023-02-09 09:49:12 +0000

    add test for deeplink page

---
+++ cypress/integration/searches-automatically-from-deeplink.spec.ts
@@ -0,0 +1,30 @@
+describe('Can search for a resident', () => {
+  beforeEach(() => {
+    cy.login();
+
+    cy.intercept('/api/evidence/residents/search', {
+      fixture: 'residents/search',
+    });
+
+    cy.visit(
+      `http://localhost:3000/teams/2/dashboard/deeplink?searchTerm=Namey`
+    );
+    cy.injectAxe();
+  });
+
+  it('Has no detectable accessibility issues', () => {
+    cy.checkA11y();
+  });
+
+  it('User can search for a resident', () => {
+    cy.get('h2').should('contain', 'Search results');
+
+    cy.get('tbody tr').should('have.length', 3);
+
+    cy.get('tbody tr:first')
+      .should('contain', 'Namey McName')
+      .and('contain', 'frodo');
+  });
+});
+
+export {};
