6ad150d1ce16845802644ce223eaf01e0f21513f
Sam Drumm <sam.drumm@hackney.gov.uk>
2025-02-26 18:29:31 +0000

    Log vars

--- src/lambda.js
+++ src/lambda.js
@@ -4,6 +4,12 @@ const files = require('serve-static');
 const path = require('path');
 const nextRequestHandler = app.getRequestHandler();
 
+console.log('Environment Variables:');
+console.log('APP_ENV:', process.env.APP_ENV);
+console.log('NEXT_PUBLIC_APP_ENV:', process.env.NEXT_PUBLIC_APP_ENV);
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('NEXT_PUBLIC_NODE_ENV:', process.env.NEXT_PUBLIC_NODE_ENV);
+
 server.use(files(path.join(__dirname, 'build')));
 server.use(files(path.join(__dirname, 'public')));
 
