4d98f4cf1480250c4a59383899abb8caa5b81ab0
Sam Drumm <sam.drumm@hackney.gov.uk>
2025-02-26 16:38:03 +0000

    Use instrumentation

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
+// const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
 
 Sentry.init({
   dsn:
@@ -17,6 +17,6 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  environment: ENVIRONMENT,
-  enabled: ENVIRONMENT === 'production' || ENVIRONMENT === 'staging',
+  // environment: ENVIRONMENT,
+  // enabled: ENVIRONMENT === 'production' || ENVIRONMENT === 'staging',
 });
