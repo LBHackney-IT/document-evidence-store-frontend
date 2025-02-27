d2acf032ad1061775137441a58ed55174c87ca94
Sam Drumm <sam.drumm@hackney.gov.uk>
2025-02-27 08:31:45 +0000

    Another one

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -8,8 +8,7 @@ import * as Sentry from '@sentry/nextjs';
 // const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
 
 const SENTRY_DSN = process.env.SENTRY_DSN;
-const SENTRY_ENVIRONMENT =
-  process.env.SENTRY_ENVIRONMENT || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
+const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT;
 const SENTRY_RELEASE = process.env.SENTRY_RELEASE;
 const NODE_ENV = process.env.NODE_ENV;
 
