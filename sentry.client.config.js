94831d359d318504873d7cd6402bd2bb4b104c1d
Sam Drumm <sam.drumm@hackney.gov.uk>
2025-02-27 15:56:33 +0000

    KMN

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,52 +4,16 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-// const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
-
-console.log('NEXT_PUBLIC_NODE_ENV client', process.env.NEXT_PUBLIC_NODE_ENV);
-console.log('NODE_ENV client', process.env.NODE_ENV);
-
-console.log(
-  'NEXT_PUBLIC_SENTRY_RELEASE client',
-  process.env.NEXT_PUBLIC_SENTRY_RELEASE
-);
-console.log('SENTRY_RELEASE client', process.env.SENTRY_RELEASE);
-
-console.log(
-  'NEXT_PUBLIC_SENTRY_ENVIRONMENT client',
-  process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT
-);
-console.log('SENTRY_ENVIRONMENT client', process.env.SENTRY_ENVIRONMENT);
-
-const ENVIRONMENT = process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
-
-console.log(
-  'NEXT_PUBLIC_SENTRY_ENVIRONMENT client',
-  process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT
-);
-console.log('SENTRY_ENVIRONMENT client', process.env.SENTRY_ENVIRONMENT);
-console.log('APP_ENV client', process.env.APP_ENV);
-console.log('NEXT_PUBLIC_APP_ENV client', process.env.NEXT_PUBLIC_APP_ENV);
-console.log('NEXT_PUBLIC_ENV client', process.env.NEXT_PUBLIC_ENV);
-console.log('ENV client', process.env.ENV);
+const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
+const SENTRY_RELEASE =
+  process.env.SENTRY_RELEASE || process.env.NEXT_PUBLIC_SENTRY_RELEASE;
+const NODE_ENV = process.env.NODE_ENV || process.env.NEXT_PUBLIC_NODE_ENV;
 
 Sentry.init({
   dsn:
     'https://10f8d5ba589f0ed60270b0e4675551b0@o183917.ingest.us.sentry.io/4508841639936001',
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-  // release: SENTRY_RELEASE,
-  // environment: SENTRY_ENVIRONMENT,
-  // enabled: NODE_ENV === 'production',
-
+  release: SENTRY_RELEASE,
+  enabled: NODE_ENV === 'production',
   environment: ENVIRONMENT,
-  integrations: [Sentry.captureConsoleIntegration()],
-  enabled:
-    ENVIRONMENT === 'production' ||
-    ENVIRONMENT === 'staging' ||
-    ENVIRONMENT === 'development',
 });
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,48 +4,17 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-// const ENVIRONMENT = process.env.NEXT_PUBLIC_ENV;
-
-// const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
-
-// const SENTRY_DSN = process.env.SENTRY_DSN;
-// const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT;
-// const SENTRY_RELEASE = process.env.SENTRY_RELEASE;
-// const NODE_ENV = process.env.NODE_ENV;
-
-console.log('NEXT_PUBLIC_NODE_ENV server', process.env.NEXT_PUBLIC_NODE_ENV);
-console.log(
-  'NEXT_PUBLIC_SENTRY_RELEASE server',
-  process.env.NEXT_PUBLIC_SENTRY_RELEASE
-);
-console.log(
-  'NEXT_PUBLIC_SENTRY_ENVIRONMENT server',
-  process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT
-);
-
-const ENVIRONMENT = process.env.NEXT_PUBLIC_ENV;
-
-console.log('NEXT_PUBLIC_ENV server', ENVIRONMENT);
+const ENVIRONMENT = process.env.APP_ENV || process.env.NEXT_PUBLIC_APP_ENV;
+const SENTRY_RELEASE =
+  process.env.SENTRY_RELEASE || process.env.NEXT_PUBLIC_SENTRY_RELEASE;
+const NODE_ENV = process.env.NODE_ENV || process.env.NEXT_PUBLIC_NODE_ENV;
 
 Sentry.init({
   dsn:
     'https://10f8d5ba589f0ed60270b0e4675551b0@o183917.ingest.us.sentry.io/4508841639936001',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-  // environment: ENVIRONMENT,
-  // enabled: ENVIRONMENT === 'production' || ENVIRONMENT === 'staging',
-  // release: SENTRY_RELEASE,
-  // environment: SENTRY_ENVIRONMENT,
-  // enabled: NODE_ENV === 'production',
 
+  tracesSampleRate: 1.0,
+  release: SENTRY_RELEASE,
+  enabled: NODE_ENV === 'production',
   environment: ENVIRONMENT,
-  integrations: [Sentry.captureConsoleIntegration()],
-  enabled:
-    ENVIRONMENT === 'production' ||
-    ENVIRONMENT === 'staging' ||
-    ENVIRONMENT === 'development',
 });
