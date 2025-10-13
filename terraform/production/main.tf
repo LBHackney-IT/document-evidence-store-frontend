3142cf375771a77162691139759370192b6f1ff4
Miles Alford <miles.alford@hackney.gov.uk>
2025-10-13 15:34:30 +0100

    Create front-end sg in DR account

---
+++ terraform/production/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 3.0"
+}
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_vpc" "dr_vpc" {
+  tags = {
+    Name = "disaster-recovery-prod"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-disaster-recovery"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/des/state"
+  }
+}
+
+resource "aws_security_group" "frontend_traffic" {
+  provider = aws.core
+
+  vpc_id      = data.aws_vpc.dr_vpc.id
+  name_prefix = "allow_frontend_traffic"
+
+  egress {
+    description = "allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
